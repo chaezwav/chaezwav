646097ac7143e865f1c706c73b077251c9997842
koehn <koehn@omg.lol>
2024-11-03 01:02:51 -0500

    Thank you copilot

--- src/lib/formatAsTitle.ts
+++ src/lib/formatAsTitle.ts
@@ -1,8 +1,21 @@
-export default function formatAsTitle(url: string) {
-  const urlpath = url.split("/").pop().split("-");
+export default function formatAsTitle(url: string): string {
+  if (!url) {
+    return "";
+  }
+
+  const urlParts = url.split("/");
+  const lastPart = urlParts.pop();
+
+  if (!lastPart) {
+    return "";
+  }
+
+  const urlpath = lastPart.split("-");
 
   for (let i = 0; i < urlpath.length; i++) {
-    urlpath[i] = urlpath[i][0].toUpperCase() + urlpath[i].substr(1);
+    if (urlpath[i]) {
+      urlpath[i] = urlpath[i][0].toUpperCase() + urlpath[i].substr(1);
+    }
   }
 
   return urlpath.join(" ");
